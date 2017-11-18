dc7a20b11b0514cabb784290e1bde7536e6e4a4f
Vasiliy <morfey.rulit@gmail.com>
2017-11-18 22:33:09 +0200

    Functions and method related to Sle moved to sle.go

--- mhelper.go
+++ mhelper.go
@@ -1,53 +1,7 @@
 package main
 
-import (
-	"errors"
-	"fmt"
-)
-
-// Sle is a structure to represent System of Linear Equations
-type Sle struct {
-	matrix    [][]float64
-	solutions []float64
-}
-
-func validateMatrix(m [][]float64) (bool, [][]float64, []float64) {
-	size := len(m)
-	var resMatrix [][]float64
-	var solutions []float64
-	for i := 0; i < size; i++ {
-		length := len(m[i])
-		if length != size+1 {
-			return false, nil, nil
-		}
-		resMatrix = append(resMatrix, m[i][:length-1])
-		solutions = append(solutions, m[i][length-1])
-	}
-	return true, resMatrix, solutions
-}
-
-//CreateSle creates System of Linear Equations from simple matrix
-func CreateSle(m [][]float64) (Sle, error) {
-	test, matrix, solutions := validateMatrix(m)
-	if test {
-		return Sle{matrix, solutions}, nil
-	}
-	return Sle{nil, nil}, errors.New("Not valid matrix passed to CreateSle")
-}
-
-//Print prints out Sle to console
-func (sle Sle) Print() {
-	for i, l := range sle.matrix {
-		j := 0
-		for ; j < len(l)-1; j++ {
-			fmt.Printf("%f, ", l[j])
-		}
-		fmt.Printf("%f | %f\n", l[j], sle.solutions[i])
-	}
-}
-
 // SwpLines swaps lines in matrix
-func SwpLines(m *[][]float64, l1, l2 int) {
+func swpLines(m *[][]float64, l1, l2 int) {
 	buf := (*m)[l1]
 	(*m)[l1] = (*m)[l2]
 	(*m)[l2] = buf
--- sle.go
+++ sle.go
@@ -1,1 +1,47 @@
 package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+// Sle is a structure to represent System of Linear Equations
+type Sle struct {
+	matrix    [][]float64
+	solutions []float64
+}
+
+func validateMatrix(m [][]float64) (bool, [][]float64, []float64) {
+	size := len(m)
+	var resMatrix [][]float64
+	var solutions []float64
+	for i := 0; i < size; i++ {
+		length := len(m[i])
+		if length != size+1 {
+			return false, nil, nil
+		}
+		resMatrix = append(resMatrix, m[i][:length-1])
+		solutions = append(solutions, m[i][length-1])
+	}
+	return true, resMatrix, solutions
+}
+
+//CreateSle creates System of Linear Equations from simple matrix
+func CreateSle(m [][]float64) (Sle, error) {
+	test, matrix, solutions := validateMatrix(m)
+	if test {
+		return Sle{matrix, solutions}, nil
+	}
+	return Sle{nil, nil}, errors.New("Not valid matrix passed to CreateSle")
+}
+
+//Print prints out Sle to console
+func (sle Sle) Print() {
+	for i, l := range sle.matrix {
+		j := 0
+		for ; j < len(l)-1; j++ {
+			fmt.Printf("%f, ", l[j])
+		}
+		fmt.Printf("%f | %f\n", l[j], sle.solutions[i])
+	}
+}
