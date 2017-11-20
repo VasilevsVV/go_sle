4001c8af4155139351411bd6f02bcfdfde5787d0
Natalia <natalia.chehunina@yandex.ua>
2017-11-20 10:04:19 +0200

    Determinant method added for matrSlice.

--- mhelper.go
+++ mhelper.go
@@ -15,14 +15,14 @@ func swpLines(m *[][]float64, l1, l2 int) {
 }
 
 // checkSquarness checks if matrix is square
-func (m matrSlice) checkSquarness() bool {
+func (m matrSlice) checkSquarness() int {
 	size := len(m)
 	for _, l := range m {
 		if len(l) != size {
-			return false
+			return -1
 		}
 	}
-	return true
+	return size
 }
 
 func (m matrSlice) checkRect() bool {
@@ -36,10 +36,7 @@ func (m matrSlice) checkRect() bool {
 }
 
 // GetMinor gets a n-th minor from matrix
-func (m matrSlice) GetMinor(n int) (matrSlice, error) {
-	if !m.checkSquarness() {
-		return nil, errors.New("Matrix is not square")
-	}
+func (m matrSlice) getMinor(n int) matrSlice {
 	var res [][]float64
 	for i, l := range m {
 		if i == n {
@@ -47,12 +44,36 @@ func (m matrSlice) GetMinor(n int) (matrSlice, error) {
 		}
 		res = append(res, l[1:])
 	}
-	return res, nil
+	return res
+}
+
+func (m matrSlice) determinant() float64 {
+	length := len(m)
+	if length == 2 {
+		return m[0][0]*m[1][1] - m[0][1]*m[1][0]
+	}
+	var res float64
+	for i, f := 0, 1.0; i < length; i, f = i+1, -f {
+		res += f * m[i][0] * m.getMinor(i).determinant()
+	}
+	return res
+}
+
+//Determinant gets determinant of matrix
+func (m matrSlice) Determinant() (float64, error) {
+	size := m.checkSquarness()
+	if size < 0 {
+		return 0, errors.New("Matrix is not square")
+	}
+	if size > 1 {
+		return m.determinant(), nil
+	}
+	return 0, errors.New("Matrix is less then 2x2")
 }
 
 // Transponate transponates matrix
 func (m matrSlice) Transponate() (matrSlice, error) {
-	if !m.checkSquarness() {
+	if m.checkSquarness() >= 0 {
 		return nil, errors.New("Matrix is not Square")
 	}
 	res := make(matrSlice, len(m), len(m))
