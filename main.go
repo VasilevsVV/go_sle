61de6ca167258bbe1bef61ce9dc56188961f0776
Natalia <natalia.chehunina@yandex.ua>
2017-11-20 20:07:03 +0200

    main

--- main.go
+++ main.go
@@ -3,7 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	sle, err := CreateSle([][]float64{{1, 2, 3, 10}, {4, 5, 6, 20}, {7, 8, 9, 30}})
+	matr := [][]float64{{1, 2, 3, 10}, {4, 5, 6, 20}, {7, 8, 9, 30}}
+	sle, err := CreateSle(matr)
 	if err == nil {
 		sle.Print()
 	} else {
@@ -11,8 +12,21 @@ func main() {
 	}
 
 	var test matrSlice = [][]float64{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}}
-	res, _ := test.GetMinor(1)
-	res.Print()
-	res1, _ := test.Transponate()
-	res1.Print()
+	res, err := test.Multm([][]float64{{10}, {20}, {30}})
+	if err == nil {
+		res.Print()
+	} else {
+		fmt.Println(err)
+	}
+
+	var bigTest matrSlice = [][]float64{{4, -1, 11, 5, 2}, {4, -2, 11, -9, 5}, {1, 2, 3, 4, 5}, {5, 4, 3, 2, 1}, {21, 3, -7, -1, 6}}
+
+	det, err := bigTest.Determinant()
+	if err == nil {
+		fmt.Printf("Det = %f\n", det)
+	} else {
+		fmt.Println(err)
+	}
+
+	bigTest.getMinor(2, 2).Print()
 }
