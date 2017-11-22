69cea7192e5d55c819ec4ec986b8e0349c9ae693
Vasiliy <morfey.rulit@gmail.com>
2017-11-22 21:50:22 +0200

    Fixed tests.

--- main.go
+++ main.go
@@ -1,17 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"./sle"
+)
 
 func main() {
 	matr := [][]float64{{1, 2, 3, 10}, {4, 5, 6, 20}, {7, 8, 9, 30}}
-	sle, err := CreateSle(matr)
+	Sle, err := sle.CreateSle(matr)
 	if err == nil {
-		sle.Print()
+		Sle.Print()
 	} else {
 		fmt.Println(err)
 	}
 
-	var test matrSlice = [][]float64{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}}
+	var test sle.MatrSlice = [][]float64{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}}
 	res, err := test.Multm([][]float64{{10}, {20}, {30}})
 	if err == nil {
 		res.Print()
@@ -19,7 +23,7 @@ func main() {
 		fmt.Println(err)
 	}
 
-	var bigTest matrSlice = [][]float64{{4, -1, 11, 5, 2}, {4, -2, 11, -9, 5}, {1, 2, 3, 4, 5}, {5, 4, 3, 2, 1}, {21, 3, -7, -1, 6}}
+	var bigTest sle.MatrSlice = [][]float64{{4, -1, 11, 5, 2}, {4, -2, 11, -9, 5}, {1, 2, 3, 4, 5}, {5, 4, 3, 2, 1}, {21, 3, -7, -1, 6}}
 
 	det, err := bigTest.Determinant()
 	if err == nil {
@@ -28,5 +32,5 @@ func main() {
 		fmt.Println(err)
 	}
 
-	bigTest.getMinor(2, 2).Print()
+	bigTest.GetMinor(2, 2).Print()
 }
