7423b796453ed6f275cc1ee2ac161f7f3cb76cae
Vasiliy <morfey.rulit@gmail.com>
2017-11-26 15:56:40 +0200

    Added tests TestMinor and TestDeterminant.

--- sle_test.go
+++ sle_test.go
@@ -7,13 +7,31 @@ import (
 )
 
 func getMatrixes() []sle.MatrSlice {
-	return []sle.MatrSlice{[][]float64{{1, 2, -3, 10}, {4, -5, 6, 20}, {-7, 8, 9, 30}},
+	return []sle.MatrSlice{
+		[][]float64{
+			{1, 2, -3, 10},
+			{4, -5, 6, 20},
+			{-7, 8, 9, 30}},
 		[][]float64{
 			{4, 7, -3, -11, 7, 36},
 			{-2, 11, 3, 21, 0, -11},
 			{0, 0, 4, -16, -5, 27},
 			{26, 0, -7, -1, 18, -5},
 			{5, 8, 21, -5, 3, 13}}}
+}
+
+func getSqrMatrixes() []sle.MatrSlice {
+	return []sle.MatrSlice{
+		[][]float64{
+			{1, 2, -3},
+			{4, -5, 6},
+			{-7, 8, 9}},
+		[][]float64{
+			{4, 7, -3, -11, 7},
+			{-2, 11, 3, 21, 0},
+			{0, 0, 4, -16, -5},
+			{26, 0, -7, -1, 18},
+			{5, 8, 21, -5, 3}}}
 }
 
 func TestSle(t *testing.T) {
@@ -58,6 +76,38 @@ func TestTransponate(t *testing.T) {
 		} else {
 			if !res.EqualTo(results[i]) {
 				t.Error("Results are not equal")
+			}
+		}
+	}
+}
+
+func TestMinor(t *testing.T) {
+	ins := getMatrixes()
+	results := []sle.MatrSlice{
+		[][]float64{{1, 2, 10}, {-7, 8, 30}},
+		[][]float64{
+			{4, 7, -11, 7, 36},
+			{0, 0, -16, -5, 27},
+			{26, 0, -1, 18, -5},
+			{5, 8, -5, 3, 13}},
+	}
+	for i, in := range ins {
+		if !in.GetMinor(1, 2).EqualTo(results[i]) {
+			t.Errorf("Result response in not equal to expected matrix")
+		}
+	}
+}
+
+func TestDeterminant(t *testing.T) {
+	ins := getSqrMatrixes()
+	results := []float64{-240, 984438}
+	for i, in := range ins {
+		res, err := in.Determinant()
+		if err != nil {
+			t.Error(err)
+		} else {
+			if res != results[i] {
+				t.Errorf("Result is not equal to expected")
 			}
 		}
 	}
