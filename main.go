d3ddbdb0d60c0656ca15d63389890f4b03e3e301
Vasiliy <morfey.rulit@gmail.com>
2017-11-26 16:04:40 +0200

    Added small demonstration to main.

--- main.go
+++ main.go
@@ -7,34 +7,18 @@ import (
 )
 
 func main() {
-	matr := [][]float64{{1, 2, -3, 10}, {4, -5, 6, 20}, {-7, 8, 9, 30}}
+	matr := [][]float64{
+		{4, 7, -3, -11, 7, 36},
+		{-2, 11, 3, 21, 0, -11},
+		{0, 0, 4, -16, -5, 27},
+		{26, 0, -7, -1, 18, -5},
+		{5, 8, 21, -5, 3, 13}}
 	Sle, err := sle.CreateSle(matr)
-	// if err == nil {
-	// 	Sle.Print()
-	// } else {
-	// 	fmt.Println(err)
-	// }
-
-	// var test sle.MatrSlice = [][]float64{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}}
-	// res, err := test.Multm([][]float64{{10}, {20}, {30}})
-	// if err == nil {
-	// 	res.Print()
-	// } else {
-	// 	fmt.Println(err)
-	// }
-
-	// var bigTest sle.MatrSlice = [][]float64{{4, -1, 11, 5, 2}, {4, -2, 11, -9, 5}, {1, 2, 3, 4, 5}, {5, 4, 3, 2, 1}, {21, 3, -7, -1, 6}}
-
-	// transp, _ := bigTest.Transponate()
-	// transp.Print()
-	// det, err := bigTest.Determinant()
-	// if err == nil {
-	// 	fmt.Printf("Det = %f\n", det)
-	// } else {
-	// 	fmt.Println(err)
-	// }
-
-	// bigTest.GetMinor(2, 2).Print()
+	if err == nil {
+		Sle.Print()
+	} else {
+		fmt.Println(err)
+	}
 
 	sleRes, err := Sle.Solve()
 	if err != nil {
@@ -42,5 +26,13 @@ func main() {
 	} else {
 		fmt.Printf("%f\n", sleRes)
 	}
-	fmt.Println(sle.CompareSlices(sleRes, []float64{7.75, 6.5, 43.0 / 12.0}))
+
+	// var bigTest sle.MatrSlice = [][]float64{
+	// 	{4, -1, 11, 5, 2},
+	// 	{4, -2, 11, -9, 5},
+	// 	{1, 2, 3, 4, 5},
+	// 	{5, 4, 3, 2, 1},
+	// }
+
+	// bigTest.Print()
 }
