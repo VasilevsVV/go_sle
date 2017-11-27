4020cec23b7ef8e78ff57a23de24629117e42273
Vasiliy <morfey.rulit@gmail.com>
2017-11-27 12:38:56 +0200

    Changed tests a bit.

--- sle_test.go
+++ sle_test.go
@@ -19,6 +19,11 @@ func getMediumMatrix() sle.MatrSlice {
 	}
 }
 
+func getMediumResult() []float64 {
+	return []float64{-0.0854486486, 1.0893912016, -0.6491091389, -0.2816926027,
+		-0.6185427379, -1.2671235885, 0.5395247648, -0.0675559173}
+}
+
 func getBigMatrix() sle.MatrSlice {
 	return [][]float64{
 		{12, 15, -3, 56, 46, 3, 56, -97, 6, 23, -9},
@@ -34,6 +39,11 @@ func getBigMatrix() sle.MatrSlice {
 	}
 }
 
+func getBigResult() []float64 {
+	return []float64{1.2106107148, -0.5511810357, 0.5321367500, -0.2105863700, -0.7306853426,
+		-0.7378783187, 1.2849789619, 0.6488343985, 1.1705003456, 0.7786916242}
+}
+
 func getBigTestMatrix() sle.MatrSlice {
 	return [][]float64{
 		{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10},
@@ -47,6 +57,33 @@ func getBigTestMatrix() sle.MatrSlice {
 		{0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 90},
 		{0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 100},
 	}
+}
+
+func getBigTestResult() []float64 {
+	return []float64{10, 20, 30, 40, 50, 60, 70, 80, 90, 100}
+}
+
+func getLargeMatrix() sle.MatrSlice {
+	return [][]float64{
+		{12, 15, -3, 56, 46, 3, 56, -97, 6, 87, 13, -9, 98},
+		{34, -8, 87, 5, 45, 6, 34, 6, -87, 6, 54, 35, -7},
+		{43, 24, -9, 8, 36, 21, 34, 67, -9, 8, 6, 4, 8},
+		{45, 6, 56, -76, 65, 42, 54, -8, -76, 53, 5, 6, -43},
+		{56, 43, 76, 5, -8, 87, 53, 24, -87, 54, 34, 3, 16},
+		{-56, 20, 43, 43, 34, -17, 72, 43, 23, -6, 13, -32, 2},
+		{76, 46, -73, 35, 85, 84, 35, 89, -65, 42, 56, 73, 91},
+		{45, 76, -73, 23, 16, 8, -43, 24, 75, 27, -81, 30, 73},
+		{4, 26, 34, 95, 27, 96, -7, -36, 75, 82, 93, 26, 75},
+		{36, 8, 35, 75, -6, 49, 76, 56, 45, 75, -65, 65, 86},
+		{56, 86, 45, 14, -63, 16, 22, 23, 25, 63, 76, -76, 56},
+		{26, -65, 87, 4, -45, 35, 75, 83, 47, 29, 64, 35, -54}}
+}
+
+func getLargeResult() []float64 {
+	return []float64{-0.3115411222, 0.0683765696, -0.3478120152,
+		0.8801386556, -0.0703200598, -0.6768418131,
+		-0.6187889943, 0.4469540338, -0.5692153340,
+		1.5853090166, -0.0647483120, 0.0012496748}
 }
 
 func getMatrixes() []sle.MatrSlice {
@@ -79,24 +116,23 @@ func getSqrMatrixes() []sle.MatrSlice {
 
 func TestSle(t *testing.T) {
 	ins := getMatrixes()
-	ins = append(ins, getBigTestMatrix())
-	ins = append(ins, getMediumMatrix())
-	ins = append(ins, getBigMatrix())
 	results := [][]float64{
 		[]float64{7.75, 6.5, float64(43.0 / 12.0)},
 		[]float64{-40237.0 / 164073.0, 58706.0 / 23439.0, -35953.0 / 54691.0,
 			-289622.0 / 164073.0, -45491.0 / 164073},
-		[]float64{10, 20, 30, 40, 50, 60, 70, 80, 90, 100},
-		[]float64{-0.0854486486, 1.0893912016, -0.6491091389, -0.2816926027,
-			-0.6185427379, -1.2671235885, 0.5395247648, -0.0675559173},
-		[]float64{1.2106107148, -0.5511810357, 0.5321367500, -0.2105863700, -0.7306853426,
-			-0.7378783187, 1.2849789619, 0.6488343985, 1.1705003456, 0.7786916242},
-		// []float64{-0.3115411222, 0.0683765696, -0.3478120152,
-		// 	0.8801386556, -0.0703200598, -0.6768418131,
-		// 	-0.6187889943, 0.4469540338, -0.5692153340,
-		// 	1.5853090166, -0.0647483120, 0.0012496748,
-		// },
 	}
+	//ins = append(ins, getBigTestMatrix())
+	// results = append(results, getBigTestResult())
+
+	ins = append(ins, getMediumMatrix())
+	results = append(results, getMediumResult())
+
+	ins = append(ins, getBigMatrix())
+	results = append(results, getBigResult())
+
+	// ins = append(ins, getLargeMatrix())
+	// results = append(results, getLargeResult())
+
 	for i, in := range ins {
 		Sle, err := sle.CreateSle(in)
 		if err != nil {
